0f1c196d02d1aa6728cbf40181f754c8361936a4
Ninu <ninuvv032@gmail.com>
2021-06-11 16:34:56 +0300

    change warning

--- src/Component/RowPost/RowPost.js
+++ src/Component/RowPost/RowPost.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./RowPost.css";
-import YouTube from 'react-youtube';
-import {API_KEY, imageUrl } from "../../constants/constants";
+import YouTube from "react-youtube";
+import { API_KEY, imageUrl } from "../../constants/constants";
 import axios from "../../axios";
 function RowPost(props) {
   const [movie, setMovie] = useState([]);
-  const [urlId, setUrlId] = useState('');
+  const [urlId, setUrlId] = useState("");
   useEffect(() => {
     axios
       .get(props.url)
@@ -16,35 +16,37 @@ function RowPost(props) {
       .catch((err) => {
         alert("Network Error");
       });
-  }, []);
+  }, [props.url]);
 
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
 
-  const movieTrailer=(movie_id)=>{
-console.log(movie_id)
-axios.get(`movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
-    if(response.data.results.length!==0){
-        setUrlId(response.data.results[0])
-    }else{
-        console.log('array is empty')
-    }
-    
-})
-  }
+  const movieTrailer = (movie_id) => {
+    console.log(movie_id);
+    axios
+      .get(`movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("array is empty");
+        }
+      });
+  };
   return (
     <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
         {movie.map((movie) => {
           return (
-            <img onClick={()=>movieTrailer(movie.id)}
+            <img
+              onClick={() => movieTrailer(movie.id)}
               className={props.isSmall ? "smallPoster_img" : "poster_img"}
               src={`${imageUrl + movie.backdrop_path}`}
               alt=""
@@ -53,8 +55,7 @@ axios.get(`movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`).then((re
         })}
       </div>
 
-    {urlId && <YouTube videoId={urlId.key} opts={opts} />} 
-
+      {urlId && <YouTube videoId={urlId.key} opts={opts} />}
     </div>
   );
 }
